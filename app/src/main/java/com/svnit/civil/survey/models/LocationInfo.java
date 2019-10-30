727491fce7f506f091810dd0c272f96a0e16013f
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2019-10-30 11:54:39 +0530

    modified location object

--- app/src/main/java/com/svnit/civil/survey/models/LocationInfo.java
+++ app/src/main/java/com/svnit/civil/survey/models/LocationInfo.java
@@ -4,107 +4,115 @@ import android.location.Location;
 import android.os.Build;
 
 public class LocationInfo {
-    private String longitude;
-    private String latitude;
-    private String speed;
-    private String altitude;
-    private String speedAccuracy;
-    private String locationAccuracy;
-    private String altitudeAccuracy;
+    private Double longitude;
+    private Double latitude;
+    private Double speed;
+    private Double altitude;
+    private Double speedAccuracy;
+    private Double locationAccuracy;
+    private Double altitudeAccuracy;
+    private Long time;
 
     public void LocationInfo() {
-        this.setLongitude("");
-        this.setLatitude("");
-        this.setSpeed("");
-        this.setAltitude("");
-        this.setSpeedAccuracy("");
-        this.setLocationAccuracy("");
-        this.setAltitudeAccuracy("");
+        this.setLongitude(0D);
+        this.setLatitude(0D);
+        this.setSpeed(0D);
+        this.setAltitude(0D);
+        this.setSpeedAccuracy(0D);
+        this.setLocationAccuracy(0D);
+        this.setAltitudeAccuracy(0D);
+        this.setTime(0L);
     }
 
-    public void LocationInfo(String longitude, String latitude, String speed, String altitude,
-                             String speedAccuracy, String locationAccuracy, String altitudeAccuracy) {
-        this.setLongitude("");
-        this.setLatitude("");
-        this.setSpeed("");
-        this.setAltitude("");
-        this.setSpeedAccuracy("");
-        this.setLocationAccuracy("");
-        this.setAltitudeAccuracy("");
+    public void LocationInfo(Double longitude, Double latitude, Double speed, Double altitude, Long time,
+                             Double speedAccuracy, Double locationAccuracy, Double altitudeAccuracy) {
+        this.setLongitude(longitude);
+        this.setLatitude(latitude);
+        this.setSpeed(speed);
+        this.setAltitude(altitude);
+        this.setSpeedAccuracy(speedAccuracy);
+        this.setLocationAccuracy(locationAccuracy);
+        this.setAltitudeAccuracy(altitudeAccuracy);
+        this.setTime(time);
     }
 
     public void LocationInfo(Location location) {
-        this.setLongitude("" + location.getLongitude());
-        this.setLatitude(Location.convert(location.getLongitude(), Location.FORMAT_DEGREES));
-        this.setSpeed("" + location.getSpeed());
-        this.setAltitude("" + location.getAltitude());
+        this.setLongitude(location.getLongitude());
+        this.setLatitude(location.getLatitude());
+        this.setSpeed(1D * location.getSpeed());
+        this.setAltitude(location.getAltitude());
         if (Build.VERSION.SDK_INT > 25 && location.hasSpeedAccuracy())
-            this.setSpeedAccuracy("" + location.getSpeedAccuracyMetersPerSecond());
+            this.setSpeedAccuracy(1D * location.getSpeedAccuracyMetersPerSecond());
         else
-            this.setSpeedAccuracy("No");
+            this.setSpeedAccuracy(0D);
         if (location.hasAccuracy())
-            this.setLocationAccuracy("" + location.getAccuracy());
+            this.setLocationAccuracy(1D * location.getAccuracy());
         else
-            this.setLocationAccuracy("No");
+            this.setLocationAccuracy(0D);
         if (Build.VERSION.SDK_INT > 25 && location.hasVerticalAccuracy())
-            this.setAltitudeAccuracy("" + location.getVerticalAccuracyMeters());
+            this.setAltitudeAccuracy(1D * location.getVerticalAccuracyMeters());
         else
-            this.setAltitudeAccuracy("No");
+            this.setAltitudeAccuracy(0D);
+        this.setTime(location.getTime());
     }
 
-    public String getLongitude() {
+    public Double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(String longitude) {
+    public void setLongitude(Double longitude) {
         this.longitude = longitude;
     }
 
-    public String getLatitude() {
+    public Double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(String latitude) {
+    public void setLatitude(Double latitude) {
         this.latitude = latitude;
     }
 
-    public String getSpeed() {
+    public Double getSpeed() {
         return speed;
     }
 
-    public void setSpeed(String speed) {
+    public void setSpeed(Double speed) {
         this.speed = speed;
     }
 
-    public String getAltitude() {
+    public Double getAltitude() {
         return altitude;
     }
 
-    public void setAltitude(String altitude) {
+    public void setAltitude(Double altitude) {
         this.altitude = altitude;
     }
 
-    public String getSpeedAccuracy() {
+    public Double getSpeedAccuracy() {
         return speedAccuracy;
     }
 
-    public void setSpeedAccuracy(String speedAccuracy) {
+    public void setSpeedAccuracy(Double speedAccuracy) {
         this.speedAccuracy = speedAccuracy;
     }
 
-    public String getLocationAccuracy() {
+    public Double getLocationAccuracy() {
         return locationAccuracy;
     }
 
-    public void setLocationAccuracy(String locationAccuracy) {
+    public void setLocationAccuracy(Double locationAccuracy) {
         this.locationAccuracy = locationAccuracy;
     }
 
-    public String getAltitudeAccuracy() {
+    public Double getAltitudeAccuracy() {
         return altitudeAccuracy;
     }
 
-    public void setAltitudeAccuracy(String altitudeAccuracy) {
+    public void setAltitudeAccuracy(Double altitudeAccuracy) {
         this.altitudeAccuracy = altitudeAccuracy;
     }
+
+    public Long getTime() { return time; }
+
+    public void setTime(Long time) { this.time = time; }
 }
